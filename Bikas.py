f7eba29beb9e372d59c6d67b062a83c4e11eb4ce
Bikas Basnet <143680053+Bikasbasnettest@users.noreply.github.com>
2024-02-01 15:55:15 +0545

    Add files via upload

---
+++ Bikas.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# hello Bikas
+
+# In[6]:
+
+
+print('Hello Bikas I love You')
+
+
+# #this is more some documentation  of the data sets of my studieds
+# 
+
+# In[9]:
+
+
+import os
+import  numpy as np
+import pandas as pd 
+
+
+# In[10]:
+
+
+data=pd.read_csv('CDD.csv')
+
+
+# In[12]:
+
+
+os.getcwd()
+
+
+# #to change the working directory of the coding
+# 
+
+# In[14]:
+
+
+os.chdir("E:\\python data anlysis code")
+
+
+# In[15]:
+
+
+data=pd.read_csv('CDD.csv')
+
+
+# In[16]:
+
+
+data
+
+
+# In[18]:
+
+
+pip install pycirclize
+
+
+# In[ ]:
+
+
+
+
